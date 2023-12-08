090e19699df4089e41d916ad1e8f0e58914d94f3
eric-dybsetter <eric.dybsetter@gmail.com>
2023-12-08 09:45:04 -0500

    Cleanup and trying dispaching events

--- plugin/injector.js
+++ plugin/injector.js
@@ -5,10 +5,21 @@ const workdayMappings = {
     city: "addressSection_city",
 };
 
+var inputevent = new Event('input', {
+    bubbles: true,
+    cancelable: true,
+});
+
+var clickevent = new Event('dblclick');
+
+var changeevent = new Event('change');
+
 data.split(',').forEach(function(field) {
     const keyVal = field.split(':');
-    fillField(document.querySelector('input[data-automation-id=' + CSS.escape(workdayMappings[keyVal[0]]) + ']'), keyVal[1]);
+    //fillField(document.querySelector('input[data-automation-id=' + CSS.escape(workdayMappings[keyVal[0]]) + ']'), keyVal[1]);
+    setReactInputValue('input[data-automation-id=' + CSS.escape(workdayMappings[keyVal[0]]) + ']', keyVal[1]);
 });
+document.ev
 
 function fillField(selector, value) {
     var field = selector();
@@ -17,6 +28,24 @@ function fillField(selector, value) {
 
 function fillField(field, value) {
     if (field) {
+        field.dispatchEvent(new Event('focus'));
         field.value = value;
+        field.dispatchEvent(new Event('input', {
+            bubbles: true,
+            cancelable: true,
+        }));
+        field.value = value;
+        field.dispatchEvent(new Event('blur'));
     }  
+}
+
+function setReactInputValue(query, elevalue) {
+    var ele = document.querySelector(query);
+    if (ele) {
+        console.log("Before Value: " + ele.value);
+        ele.value = elevalue;
+        ele.dispatchEvent(new Event('blur'));
+        console.log("After Value: " + ele.value);
+
+    }
 }
