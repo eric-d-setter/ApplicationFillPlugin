481ffd6a37c1da3401f10541f6c1f1e9a26326ac
eric-dybsetter <eric.dybsetter@gmail.com>
2023-12-12 15:44:58 -0500

    cleaning up

--- plugin/injector.js
+++ plugin/injector.js
@@ -5,51 +5,26 @@ const workdayMappings = {
     city: "addressSection_city",
 };
 
-var inputevent = new Event('input', {
-    bubbles: true,
-    cancelable: true,
-});
-
-function changevalue(element, stringval) {
-    element.value = stringval;
-    element.dispatchEvent(inputevent);
-}
-
-var clickevent = new Event('dblclick');
-
-var changeevent = new Event('change');
-
 data.split(',').forEach(function(field) {
     const keyVal = field.split(':');
-    //fillField(document.querySelector('input[data-automation-id=' + CSS.escape(workdayMappings[keyVal[0]]) + ']'), keyVal[1]);
-    setReactInputValue('input[data-automation-id=' + CSS.escape(workdayMappings[keyVal[0]]) + ']', keyVal[1]);
+    fillField(document.querySelector('input[data-automation-id=' + CSS.escape(workdayMappings[keyVal[0]]) + ']'), keyVal[1]);
+    //setReactInputValue('input[data-automation-id=' + CSS.escape(workdayMappings[keyVal[0]]) + ']', keyVal[1]);
 });
-document.ev
 
 function fillField(selector, value) {
     var field = selector();
-    fillField(field, value);
+    field.addEventListener("change", fillField(field, value));
 }
 
 function fillField(field, value) {
     if (field) {
-        field.dispatchEvent(new Event('focus'));
+        field.focus();
         field.value = value;
         field.dispatchEvent(new Event('input', {
             bubbles: true,
             cancelable: true,
         }));
         field.value = value;
-        field.dispatchEvent(new Event('blur'));
+        field.blur();
     }  
-}
-
-function setReactInputValue(query, elevalue) {
-    var ele = document.querySelector(query);
-    if (ele) {
-        console.log("Before Value: " + ele.value);
-        changevalue(ele, elevalue)
-        ele.dispatchEvent(new Event('blur'));
-        console.log("After Value: " + ele.value);
-    }
 }
